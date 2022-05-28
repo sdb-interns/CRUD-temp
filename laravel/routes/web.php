2f42a7fb58e428901f0c72f8c68e32553b2048c1
SATOTOMI <tomita@i.social-db.co.jp>
2022-05-28 17:45:40 +0900

    practice用に色々削除

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -16,17 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-//todo:名前付きルート
-Route::get('user/index','UserController@index')->name('user.index');
-Route::get('user/create','UserController@create')->name('user.create');
-Route::post('user/store','UserController@store')->name('user.store');
-Route::get('user/edit/{id}','UserController@edit')->name('user.edit');
-Route::post('user/update/{id}','UserController@update')->name('user.update');
-Route::delete('user/delete/{id}','UserController@delete')->name('user.delete');
 
-Route::get('post/index','PostController@index')->name('post.index');
-Route::get('post/create','PostController@create')->name('post.create');
-Route::post('post/store','PostController@store')->name('post.store');
-Route::get('post/edit/{id}','PostController@edit')->name('post.edit');
-Route::post('post/update/{id}','PostController@update')->name('post.update');
-Route::delete('post/delete/{id}','PostController@delete')->name('post.delete');
